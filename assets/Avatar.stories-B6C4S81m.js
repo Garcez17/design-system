26399d6c15566f8b75136498e3afbb6e27da347b
Garcez17 <Garcez17@users.noreply.github.com>
2025-05-30 17:43:44 +0000

    deploy: 5401518c4d0505c7baf4e3a9aa4ce5514307323e

---
+++ assets/Avatar.stories-B6C4S81m.js
@@ -0,0 +1,6 @@
+import{A as p}from"./index-CveeVDNj.js";import"./index-CgfFrydU.js";import"./_commonjsHelpers-CqkleIqs.js";import"./index-lFQ-1PWb.js";import"./index-B2KKL2uI.js";const g={title:"Data display/Avatar",component:p,parameters:{docs:{description:{component:"O avatar é usado para exibir uma imagem de perfil do usuário."}}},args:{src:"https://github.com/garcez17.png",alt:"Gabriel Garcez"},argTypes:{src:{control:{type:"text"}}}},r={},a={args:{src:void 0,alt:"Gabriel Garcez"}};var e,t,s;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:"{}",...(s=(t=r.parameters)==null?void 0:t.docs)==null?void 0:s.source}}};var o,c,i;a.parameters={...a.parameters,docs:{...(o=a.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    src: undefined,
+    alt: 'Gabriel Garcez'
+  }
+}`,...(i=(c=a.parameters)==null?void 0:c.docs)==null?void 0:i.source}}};const b=["Primary","WithFallback"];export{r as Primary,a as WithFallback,b as __namedExportsOrder,g as default};
